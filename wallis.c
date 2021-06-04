f771c4eb3638e11a1bb225bf89a5422f8f236d21
Jarupula-Priya <84661913+Jarupula-Priya@users.noreply.github.com>
2021-06-04 12:27:30 +0530

    Wallis.c

--- wallis.c
+++ wallis.c
@@ -1,10 +1,11 @@
+
+
+
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
 float wallis_pi(int);
-
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
@@ -14,7 +15,6 @@ int main(void) {
       abort();
     }
   }
-
   for (int i=500; i<3000; i++) {
     pi = wallis_pi(i);
     if (!(fabs(pi - M_PI) < 0.01)) {
@@ -23,4 +23,12 @@ int main(void) {
     }
   }
 }
-
+float wallis_pi(int n){	
+float count =1.0;
+float j;
+for(int i=1;i<=(int)n;i++){
+ j=(float)4.0*i*i/((4.0*i*i)-1);
+  count*=j;
+}
+return count*2;
+}
